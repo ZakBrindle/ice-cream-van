bc480eeee78b8d359cf03ca0916c906c6d5bbc18
Zak Brindle <z4kbrindle@gmail.com>
2024-11-10 22:39:38 +0000

    firebase config

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -2,7 +2,7 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
