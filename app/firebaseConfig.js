b743b675eb526ac11b5fb7870c93b2ab6ec9c63c
Zak Brindle <z4kbrindle@gmail.com>
2024-11-10 22:47:28 +0000

    projID

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -4,7 +4,7 @@
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  projectId: "ice-cream-van-a74d9",
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
