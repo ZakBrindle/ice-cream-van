95c08bdee296bc04655a6a99e0d2311fed9669a7
Zak Brindle <z4kbrindle@gmail.com>
2024-11-23 20:05:39 +0000

    abort, abort. revert! 2

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,8 +1,5 @@
 
 // Your web app's Firebase configuration
-
-import { getAuth } from "firebase/auth";
-
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_CONFIG,
@@ -17,6 +14,4 @@ console.log(process.env);
 console.log(firebaseConfig);
 
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app); 
 export default firebaseConfig;
