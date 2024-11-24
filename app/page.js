e201a7e1e1923eb441928cd44748ec03414c920c
Zak Brindle <z4kbrindle@gmail.com>
2024-11-24 12:49:44 +0000

    update head with icon

--- app/page.js
+++ app/page.js
@@ -14,58 +14,61 @@ const provider = new GoogleAuthProvider();
 
 export default function Home() {
 
-    const signInWithGoogle = async () => {
-        try {
-            const result = await signInWithPopup(auth, provider);
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
-            console.log(user); 
-            window.location.href = "/map";
-            // You can access user.uid, user.displayName, user.email, etc.
-            
-            // TODO: Store user data in your database or state management solution
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(user);
+      window.location.href = "/map";
+      // You can access user.uid, user.displayName, user.email, etc.
 
-        } catch (error) {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            console.error("Error signing in with Google:", error);
-        }
-    };
+      // TODO: Store user data in your database or state management solution
 
-    return (
-        <div className={styles.app}>
-            <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" />
-            <div className={styles.loginPage}>
-                <Head>
-                    <title>Ice Cream Van</title>
-                </Head>
-                <img src="https://i.imgur.com/SvGSF6I.png" alt="Logo" className={styles.loginPageLogo} />
-                <h2>Ice Cream Van</h2>
-                <br />
-                <input type="text" id="username" placeholder="Username" className={styles.inputField} />
-                <input type="password" id="password" placeholder="Password" className={styles.inputField} />
-                <button onClick={login} className={styles.loginButton}>Login</button>
-                <hr className={styles.separator} /> {/* Separator line */}
-                <button onClick={signInWithGoogle} className={styles.loginButton}>Sign in with Google</button>
-                <button onClick={guestLogin} className={styles.loginButton}>Sign in as Guest</button>
-                <br />
-            </div>
-        </div>
-    );
+    } catch (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.error("Error signing in with Google:", error);
+    }
+  };
+
+  return (
+    <div className={styles.app}>
+      <div className={styles.loginPage}>
+
+        <Head>
+          <title>Ice Cream Van</title>
+          <link rel="icon" href="./logo_nobg.ico" />
+          <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+        </Head>
+
+        <img src="https://i.imgur.com/SvGSF6I.png" alt="Logo" className={styles.loginPageLogo} />
+        <h2>Ice Cream Van</h2>
+        <br />
+        <input type="text" id="username" placeholder="Username" className={styles.inputField} />
+        <input type="password" id="password" placeholder="Password" className={styles.inputField} />
+        <button onClick={login} className={styles.loginButton}>Login</button>
+        <hr className={styles.separator} /> {/* Separator line */}
+        <button onClick={signInWithGoogle} className={styles.loginButton}>Sign in with Google</button>
+        <button onClick={guestLogin} className={styles.loginButton}>Sign in as Guest</button>
+        <br />
+      </div>
+    </div>
+  );
 }
 
 function login() {
-    // ... (Authentication logic)
-    window.location.href = "/app";
+  // ... (Authentication logic)
+  window.location.href = "/app";
 }
 
 function guestLogin() {
-    // ... (Guest login logic)
-    window.location.href = "/map";
+  // ... (Guest login logic)
+  window.location.href = "/map";
 }
